782c277dd44751796df3750583a88b7aa6b4f672
ArvinCS <arvinciu86@yahoo.com>
2023-09-20 12:42:17 +0700

    feat: update tutorial 2 & 3 (telat gemastik)

--- main/models.py
+++ main/models.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=255)
     date_added = models.DateField(auto_now_add=True)
     price = models.IntegerField()
     description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
